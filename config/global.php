e8996997f8ed102c84482d90d7bcaf55d3088ba2
Jonathan Javier Sánchez Sanabria <jjss9652@gmail.com>
2018-05-19 18:01:57 -0500

    configuracion conexion bd

--- config/global.php
+++ config/global.php
@@ -1,21 +1,19 @@
-<?php
-
-//ip de la pc que será servidor de la bd
+<?php 
+//Ip de la pc servidor de base de datos
 define("DB_HOST","localhost");
 
-//nombre de la bd
-define("DB_NAME","dbsistema");
+//Nombre de la base de datos
+define("DB_NAME", "dbsistema");
 
-//usuario de la bd
-define("DB_USERNAME","root");
+//Usuario de la base de datos
+define("DB_USERNAME", "root");
 
-//contraseña bd
-define("DB_PASSWORD","");
+//ContraseÃ±a del usuario de la base de datos
+define("DB_PASSWORD", "");
 
-//codificacion de caracteres
+//definimos la codificaciÃ³n de los caracteres
 define("DB_ENCODE","utf8");
 
-//constante como nombre del proyecto
+//Definimos una constante como nombre del proyecto
 define("PRO_NOMBRE","ITVentas");
-
 ?>
