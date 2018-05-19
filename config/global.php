c3e515681aacb69bb91994c6f73055e5923e2df9
Jonathan Javier Sánchez Sanabria <jjss9652@gmail.com>
2018-05-19 16:51:43 -0500

    archivo global para conexion a bd

---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php
+
+//ip de la pc que será servidor de la bd
+define("DB_HOST","localhost");
+
+//nombre de la bd
+define("DB_NAME","dbsistema");
+
+//usuario de la bd
+define("DB_USERNAME","root");
+
+//contraseña bd
+define("DB_PASSWORD","");
+
+//codificacion de caracteres
+define("DB_ENCODE","utf8");
+
+//constante como nombre del proyecto
+define("PRO_NOMBRE","ITVentas");
+
+?>
---
+++ scriptbd.sql
@@ -0,0 +1,1 @@
+SELECT * FROM dbsistema.articulo;
